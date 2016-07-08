b72efead3c4d2a570fa9f047c8c3e34543685070
Juraci Vieira <juraci.vieira@gmail.com>
2016-07-08 18:22:30 -0300

    Improving the example

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    actions: {
+        showOnMap() {
+            Ember.Logger.warn('Mouseenter');
+            // Somehow trigger infowindow.open() for the marker context in the map
+        }
+    }
 });
